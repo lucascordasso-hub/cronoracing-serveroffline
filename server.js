df726678c1736603df81e38d821a662bff16db30
Lucas Cordasso <lucaseliasrock@gmail.com>
2026-08-17 11:43:54 -0300

    Up

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ const rotasCatalogos = require('./routes/catalogos');
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: '50mb' })); 
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '100mb' })); 
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 // ==========================================
 // CONFIGURAÇÃO DO FRONTEND
